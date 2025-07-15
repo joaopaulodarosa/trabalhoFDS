5bd72b7c0f77a72ea380a549003b4d7a541e6249
joaopaulodarosa <joaodarosa2304@gmail.com>
2025-07-14 22:39:03 -0300

    Add files via upload

---
+++ MEU SITE/JS/script.js
@@ -0,0 +1,7 @@
+let botao = document.querySelector("#botao");
+let mensagem = document.querySelector("#mensagem");
+
+botao.addEventListener("click", e => {
+    mensagem.innerHTML = "Sua vida é muito curta para que fique carrancudo com tudo e todos, com raiva, nervoso, respire bem fundo e aproveite a vida enquanto você ainda a tem!";
+    
+});
